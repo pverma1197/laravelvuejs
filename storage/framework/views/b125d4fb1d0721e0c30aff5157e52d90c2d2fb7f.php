bc4d8646f12612774cc753cfcf4163b608c8b0f1
pverma1197 <vermapooja1197@gmail.com>
2020-08-15 12:20:10 +0530

    laravel+vue

---
+++ storage/framework/views/b125d4fb1d0721e0c30aff5157e52d90c2d2fb7f.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+        <script>window.Laravel = { csrfToken: '<?php echo e(csrf_token()); ?>'}</script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+        <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
+    </head>
+    <body>
+       <div id="app">
+            <navbar></navbar>
+            <div class="container">
+                <article-component></artcile-component>
+            </div>  
+        </div>
+       
+    </body>
+</html><?php /**PATH C:\xamppserver\htdocs\laravel\vueproject\resources\views/demo.blade.php ENDPATH**/ ?>
